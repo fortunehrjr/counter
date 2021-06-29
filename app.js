a854aa6934293704c0d3fa0918a3b36e3bc7e475
fortunehrjr <83907042+fortunehrjr@users.noreply.github.com>
2021-06-30 01:09:27 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,21 @@
+let countEl = document.getElementById("count-el")
+
+let count = 0
+
+
+function increment() {
+    count += 1 
+    countEl.textContent= count
+    console.log(count)
+}
+
+let saveEl = document.getElementById("save-el")
+
+
+function save() {
+    let countStr= count + " - "
+    saveEl.textContent += countStr
+    countEl.textContent = 0
+    count = 0
+}
+
